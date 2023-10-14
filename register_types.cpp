adede2162aee9edd8860e0feb301b5f6cbacc67d
DigitOtter <digitotter@protonmail.com>
2023-10-14 23:57:43 +0200

    Removed unused files

--- register_types.cpp
+++ register_types.cpp
@@ -2,9 +2,6 @@
 
 #include <drivers/vulkan/vulkan_context.h>
 
-// #include "godot_texture_share.h"
-// #include "texture_sender.h"
-
 void initialize_gd_module_texture_share_vk_module(ModuleInitializationLevel p_level) {
 	if (p_level == MODULE_INITIALIZATION_LEVEL_CORE) {
 		WARN_PRINT("Loading additional Vulkan extensions");
@@ -16,9 +13,6 @@ void initialize_gd_module_texture_share_vk_module(ModuleInitializationLevel p_le
 		VulkanContext::register_requested_device_extension(VK_KHR_TIMELINE_SEMAPHORE_EXTENSION_NAME, true);
 		VulkanContext::register_requested_device_extension(VK_KHR_EXTERNAL_MEMORY_FD_EXTENSION_NAME, true);
 		VulkanContext::register_requested_device_extension(VK_KHR_EXTERNAL_SEMAPHORE_FD_EXTENSION_NAME, true);
-	} else if (p_level == MODULE_INITIALIZATION_LEVEL_SCENE) {
-		// ClassDB::register_class<GodotTextureShare>();
-		// ClassDB::register_class<TextureSender>();
 	}
 }
 
